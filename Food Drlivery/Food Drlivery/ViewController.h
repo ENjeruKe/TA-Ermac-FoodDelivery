19a56d4eddb6145a1bab0c24bb2d5a0c9de47bce
Admin <admin@Mac-Admin-2.local>
2014-11-05 13:43:12 +0200

    project started...

---
+++ Food Drlivery/Food Drlivery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Food Drlivery
+//
+//  Created by Admin on 11/5/14.
+//  Copyright (c) 2014 Borislav Boyadzhiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Food Drlivery/Food Drlivery/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Food Drlivery
+//
+//  Created by Admin on 11/5/14.
+//  Copyright (c) 2014 Borislav Boyadzhiev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
