5bc95bb3839139878ad81adaa0cc270df45eb67f
etcet1 <e_grekov@abv.bg>
2014-11-08 15:27:01 +0200

    added profile view

---
+++ Food Drlivery/Food Drlivery/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Food Drlivery
+//
+//  Created by user on 08/11/14.
+//  Copyright (c) 2014 Borislav Boyadzhiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
