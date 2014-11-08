7e1ebab5c793119cd232e6f59da0d5e9944752ea
Borislav Boyadzhiev <bboyadzhiev@Borislavs-Mac.local>
2014-11-08 09:39:44 -0800

    profile photo fetching fixed

--- Food Drlivery/Food Drlivery/ProfileViewController.m
+++ Food Drlivery/Food Drlivery/ProfileViewController.m
@@ -18,19 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    PFQuery* queryPhoto = [PFQuery queryWithClassName:@"User"];
-    [queryPhoto whereKey:@"user" equalTo:[PFUser currentUser]];
-    
+    //PFQuery* queryPhoto = [PFQuery queryWithClassName:@"_User"];
+    PFQuery *queryPhoto = [PFUser query];
+    [queryPhoto whereKey:@"objectId" equalTo:[PFUser currentUser].objectId];
     [queryPhoto findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         // Verify if there are no errors
         if (!error) {
-            
+             if (objects.count > 0) {
+                  NSLog(@"NO ERROR");
             PFObject* profilePhotoObject = objects[0];
+             //   NSLog(@"%@", profilePhotoObject);
             PFFile* currentUserPhoto = (PFFile *)[profilePhotoObject objectForKey:@"profilePic"];
-            UIImageView* currentUserImage = [[UIImageView alloc]initWithImage:[UIImage imageWithData:currentUserPhoto.getData]];
-            self.profilePic = currentUserImage;
-
+           // UIImageView* currentUserImage = [[UIImageView alloc]initWithImage:[UIImage imageWithData:currentUserPhoto.getData]];
+           // self.profilePic = currentUserImage;
+                 self.profilePic.image =[UIImage imageWithData:currentUserPhoto.getData];
+             }
         }else {
+            NSLog(@"ERROR!!!");
             NSString *errorString = [[error userInfo] objectForKey:@"error"];
             UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
             [errorAlertView show];
